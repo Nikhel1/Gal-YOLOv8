f0d693b3347b70280c035d4c9fb388f891a12785
Nikhel <ngupta@usm.lmu.de>
2023-11-21 09:42:16 +0000

    Update train.py

--- YOLO_RadioGalaxyNET/train.py
+++ YOLO_RadioGalaxyNET/train.py
@@ -5,7 +5,7 @@ model = YOLO("yolov8n.yaml")  # build a new model from scratch
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="RadioGalaxyNET.yaml", epochs=20)  # train the model
+model.train(data="RadioGalaxyNET.yaml", epochs=50)  # train the model
 
 print ('Validation set metrics ------------------------------------------------:')
 metrics = model.val()
